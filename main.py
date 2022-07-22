65dea1f20f39f186182332dceb3f50834634e22d
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-07-22 11:54:59 +0200

    Add multiprocessing in main.

---
+++ main.py
@@ -0,0 +1,26 @@
+import time 
+from multiprocessing import Pool
+from utilities import *
+
+if __name__ == "__main__":
+    nmbr_samples = 1e6
+    nmbr_parallel_blocks = 4
+    pool = Pool(processes=nmbr_parallel_blocks)
+    nmbr_samples_per_worker = nmbr_samples / nmbr_parallel_blocks
+    print(f"Samples per worker {nmbr_samples_per_worker}\n\n")
+
+    nmbr_trials_per_process = [nmbr_samples_per_worker] * nmbr_parallel_blocks
+
+    print("Multiprocess version:")
+    t1 = time.time()
+    nbr_in_quarter_unit_circles = pool.map(monte_carlo_hits_numb, nmbr_trials_per_process)
+    pi_estimate = sum(nbr_in_quarter_unit_circles) * 4 / float(nmbr_samples)
+    print(f"Estimated pi {pi_estimate}")
+    print(f"Time delta: {time.time() - t1}")
+
+    print("\n\nOne process version: ")
+    t1 = time.time()
+    hits = monte_carlo_hits_numb(nmbr_samples)
+    pi_estimate = hits * 4 / float(nmbr_samples)
+    print(f"Estimated pi {pi_estimate}")
+    print(f"Time delta: {time.time() - t1}")
